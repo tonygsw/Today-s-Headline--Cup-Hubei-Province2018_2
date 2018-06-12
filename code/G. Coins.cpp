06bbc7cef84acd02116dcea0e53d00f5a3605bc2
tony <tonygsw@github.com>
2018-06-12 18:01:11 +0800

    add G

---
+++ code/G. Coins.cpp
@@ -0,0 +1,21 @@
+/*
+    author:gsw
+    data:2018.05.02
+    link:https://www.nowcoder.com/acm/contest/104/F
+    accout:tonygsw
+*/
+#define ll long long
+#define IO ios::sync_with_stdio(false);
+
+#include<math.h>
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
